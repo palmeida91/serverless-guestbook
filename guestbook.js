43c90b060219628535f9cfdb5b4ce7a483e1627b
Paulo Almeida <paulo.vitor102@gmail.com>
2022-08-15 09:51:32 +0000

    added api route

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0f5c357b.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
